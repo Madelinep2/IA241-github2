ad6ccce7d5922b697d5fb51fae01b37fd70c6587
Madeline <psimasmm@dukes.jmu.edu>
2022-03-01 19:33:34 +0000

    lab5

--- lab5.py
+++ lab5.py
@@ -1,1 +1,55 @@
-print("hello world")
+'''
+lab5 if statement 
+'''
+
+#3.1
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you just earned 5 points')
+    
+   #3.2
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('you just earned 5 points')
+    
+else:
+    print('you just earned 10 points')
+    
+#3.3
+
+favorite_fruits = ['pineapple', 'orange','banana']
+
+if 'banana' in favorite_fruits:
+    print('you really like banana')
+
+if 'grapes' in favorite_fruits:
+    print('you really like banana')
+    
+if 'mango' in favorite_fruits:
+    print('you really like banana')
+    
+#3.4
+
+age = 23
+
+#if age < 10: 
+ #   print('you are a kid')
+#elif age <20:
+#    print('you are a teenager')
+#else:
+ #   print('you are an adult')
+ #   if age >65:
+ #       print('you are an elder')
+        
+if age >=20:
+    print ('you are an adult')
+    if age >65:
+        print('you are an elder')
+elif age >10:
+        print('you are a teenager')
+else:
+    print('you are a kid')
