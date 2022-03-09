45e1db1c3dc3ad07de41dff46e7988b15a36bb28
Madeline <psimasmm@dukes.jmu.edu>
2022-03-09 02:29:01 +0000

    lab6

---
+++ letcure6.py
@@ -0,0 +1,27 @@
+'''
+lec6
+'''
+
+for letter in ['a','b','c']:
+    
+    print(letter)
+    
+    demo_str = 'this is my string'
+    
+    for c in demo_str:
+        
+        print(c)
+        
+    for word in demo_str.split():
+        
+        print(word)
+    for i in range(5):
+        
+        print(i)
+    
+    for i in range(1,5):
+        print(i)
+        
+    for i in range(1,5,2):
+        print(i)
+        
